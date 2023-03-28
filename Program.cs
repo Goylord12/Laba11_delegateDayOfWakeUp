3ee2c5a194ca71f937dac71bc3f6246e838de4af
Goylord12 <Renden15@yandex.ru>
2023-03-28 12:26:20 +0300

    fgfdg

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    delegate string GetNextDayOfWeek();
+
+    static void Main()
+    {
+      
+        int currentDayIndex = (int)DateTime.Today.DayOfWeek;
+        if (currentDayIndex != 1) currentDayIndex -= 1;
+
+        GetNextDayOfWeek getNextDay = () =>
+        {
+            currentDayIndex = (currentDayIndex + 1);
+            switch (currentDayIndex)
+            {
+                case 1:
+                    return "Понедельник";
+                case 2:
+                    return "Вторник";
+                case 3:
+                    return "Среда";
+                case 4:
+                    return "Четверг";
+                case 5:
+                    return "Пятница";
+                case 6:
+                    return "Суббота";
+                case 7:
+                    return "Воскресенье";
+                default:
+                    currentDayIndex = 1;
+                    return "Понедельник";
+            }
+
+        };
+
+        for (int i = 0; i < 14; i++)
+        {
+            Console.WriteLine(getNextDay());
+        }
+    }
+}
